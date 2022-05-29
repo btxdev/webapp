938b74f5727a35c1e2cc337aec136bc7f431436c
Mihail Batalov <limecompro@gmail.com>
2022-05-29 15:12:24 +0500

    Add title

--- auth.php
+++ auth.php
@@ -11,7 +11,7 @@ $html_title = $settings->get('html_title');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $html_title ?> : Авторизация</title>
 </head>
 <body>
     
