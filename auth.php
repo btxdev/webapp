1bb2cee4f87ef644cceadee4ac4e62914bbe2718
Mihail Batalov <limecompro@gmail.com>
2022-05-29 15:21:18 +0500

    Add auth.php

--- auth.php
+++ auth.php
@@ -11,9 +11,13 @@ $html_title = $settings->get('html_title');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Авторизация :: <?= $html_title ?></title>
+    <link rel="stylesheet" href="css/auth.css">
+    <script type="application/javascript" src="js/auth.js"></script>
 </head>
 <body>
-    
+    <div class="container">
+
+    </div>
 </body>
 </html>
